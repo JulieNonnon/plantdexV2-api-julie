250f259c4247c614fe122b0ce32fe930d109d4c1
JulieNonnon <julie.nonnon@gmail.com>
2024-04-08 00:06:58 +0200

    implemented PlantRoutes

--- src/routes/PlantRoutes.ts
+++ src/routes/PlantRoutes.ts
@@ -1,1 +1,27 @@
 import { Router } from "express";
+import PlantController from "../controllers/PlantControllers";
+
+const plantRouter = Router();
+const plantController = new PlantController();
+
+plantRouter.get("/", (req, res) => {
+  plantController.getAll(req, res);
+});
+
+plantRouter.get("/:id", (req, res) => {
+  plantController.getById(req, res);
+});
+
+plantRouter.post("/", (req, res) => {
+  plantController.create(req, res);
+});
+
+plantRouter.put("/:id", (req, res) => {
+  plantController.update(req, res);
+});
+
+plantRouter.delete("/:id", (req, res) => {
+  plantController.delete(req, res);
+});
+
+export default plantRouter; //sera appelé par l'index,
